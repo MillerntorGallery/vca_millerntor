f5f82ffc22a5a0e08c1b0626b381f832b49e0701
Sebastian Fuchs <sfuchs@projektkater.de>
2016-06-24 13:55:57 +0200

    changes to theme

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,9 +5,9 @@ if (!defined('TYPO3_MODE')) {
 
 # Entwicklungsumgebung
 # ExtBase Reflection Cache AUS
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extbase_reflection']['backend'] = 't3lib_cache_backend_NullBackend';
+#$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extbase_reflection']['backend'] = 't3lib_cache_backend_NullBackend';
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extbase_object']['backend'] = 't3lib_cache_backend_NullBackend';
+#$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extbase_object']['backend'] = 't3lib_cache_backend_NullBackend';
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	'VCA.' . $_EXTKEY,
